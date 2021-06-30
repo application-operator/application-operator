feaeb7ea3629d47f0a6d7dd0ed64119980a0ea7b
Ashley Davis <adavis@skedulo.com>
2021-06-30 10:11:56 +1000

    Fixed json name for the Deployment field.

--- api/v1alpha1/application_types.go
+++ api/v1alpha1/application_types.go
@@ -38,7 +38,7 @@ type ApplicationSpec struct {
 	// You could set this to a commit has or label.
 	// +optional
 	//
-	Deployment *string `json:"deploymentId,omitempty"`
+	Deployment *string `json:"deployment,omitempty"`
 }
 
 // ApplicationStatus defines the observed state of Application
